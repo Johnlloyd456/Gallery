a4e3143dcc06e3d292b8182cf3c6bf8d82be57dc
Johnlloyd456 <youremail@example.com>
2025-11-27 07:42:58 -0330

    First commit

---
+++ slamdunk.js
@@ -0,0 +1,57 @@
+var slamdunk = [
+    {
+        image: "SakuragiS.jpg",
+        name: "Hanamichi Sakuragi",
+        description: "Shohoku Rebound King - Forward"
+    },
+    {
+        image: "akagi.jpg",
+        name: "Takenori Akagi",
+        description: "Shohoku Captain - Center"
+    },
+    {
+        image: "Rukawa.jpg",
+        name: "Kaede Rukawa",
+        description: "Shohoku Ace - Small Forward"
+    },
+    {
+        image: "Mitsui.jpg",
+        name: "Hisashi Mitsui",
+        description: "Shohoku Shooting Guard"
+    },
+    {
+        image: "miyagi.jpg",
+        name: "Ryota Miyagi",
+        description: "Shohoku Point Guard"
+    },
+    {
+        image: "Kogure.jpg",
+        name: "Kiminobu Kogure",
+        description: "Shohoku Vice-Captain - Forward"
+    },
+    {
+        image: "Anzai.jpg",
+        name: "Mitsuyoshi Anzai",
+        description: "Shohoku Coach"
+    },
+    {
+        image: "Ayako.jpg",
+        name: "Ayako",
+        description: "Shohoku Manager"
+    },
+    {
+        image: "haruko.jpg",
+        name: "Haruko Akagi",
+        description: "Sakuragi Girlfriend"
+    },
+    {
+        image: "Makii.jpg",
+        name: "Shinichi Maki",
+        description: "Kainan Captain - Guard"
+    },
+    {
+        image: "Akira Sendou.jpg",
+        name: "Akira Sendoh",
+        description: "Ryonan Ace player - Forward"
+    }
+];
